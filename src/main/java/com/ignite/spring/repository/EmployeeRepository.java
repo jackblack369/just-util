f33f9eada0a52e0baa4c0a926b631c0ea38d593e
dongwei <jackblack369@163.com>
2023-09-28 14:52:41 +0800

    add ignite sql

--- src/main/java/com/ignite/spring/repository/EmployeeRepository.java
+++ src/main/java/com/ignite/spring/repository/EmployeeRepository.java
@@ -1,8 +1,8 @@
 package com.ignite.spring.repository;
 
 import com.ignite.spring.dto.EmployeeDTO;
-import org.apache.ignite.springdata.repository.IgniteRepository;
-import org.apache.ignite.springdata.repository.config.RepositoryConfig;
+import org.apache.ignite.springdata22.repository.IgniteRepository;
+import org.apache.ignite.springdata22.repository.config.RepositoryConfig;
 import org.springframework.stereotype.Repository;
 
 @Repository
